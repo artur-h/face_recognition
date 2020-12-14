2fcb7f713550696acd07d5fd2d92f2bb22db8cf8
Artur Hlushko <Artur_Hlushko@epam.com>
2020-12-14 10:32:09 +0200

    implemented face detection

--- script.js
+++ script.js
@@ -4,21 +4,45 @@ const loadLabeledImages = () => {
   const labels = ['James', 'Jeremy', 'Richard'];
 
   return Promise.all(labels.map(async (label) => {
+    const descriptions = [];
 
+    for (let i = 1; i <= 2; i++) {
+      try {
+        const img = await faceapi.fetchImage(`https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/artur-h/face_recognition/master/labeled-images/${label}/${i}.jpg`);
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+
+    return new faceapi.LabeledFaceDescriptors(label, descriptions);
   }));
 };
 
-const start = () => {
+const start = async () => {
+  let image;
+  let canvas;
   const container = document.createElement('div');
   container.style.position = 'relative';
 
   document.body.append(container);
+
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+
   document.body.append('Loaded');
 
   imageUpload.addEventListener('change', async () => {
-    // saving uploaded image to work with faceapi
-    const image = await faceapi.bufferToImage(imageUpload.files[0]);
-    const canvas = faceapi.createCanvasFromMedia(image);
+    if (image) image.remove();
+    if (canvas) canvas.remove();
+
+    image = await faceapi.bufferToImage(imageUpload.files[0]);
+    canvas = faceapi.createCanvasFromMedia(image);
+
     const displaySize = {
       width: image.width,
       height: image.height,
@@ -35,10 +59,11 @@ const start = () => {
       .withFaceLandmarks()
       .withFaceDescriptors();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    const results = resizedDetections.map((detection) => faceMatcher.findBestMatch(detection.descriptor));
 
-    resizedDetections.forEach((detection) => {
-      const box = detection.detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' });
+    results.forEach((result, index) => {
+      const box = resizedDetections[index].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() });
       drawBox.draw(canvas);
     })
   });
